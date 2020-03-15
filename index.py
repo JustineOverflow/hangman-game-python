a8de5bffbf39e236cb1573d6654569d1cec78b5f
Justine Fabre <justinefabre84@gmail.com>
2020-03-15 16:57:24 +0000

    code adjustements

--- index.py
+++ index.py
@@ -1,17 +1,3 @@
-def check(a, b):
-    if a == b:
-        return True
-    if a is None or b is None:
-        return False
-    if len(a) != len(b):
-        return False
-
-    for i in a:
-        if i == i in b:
-            return False
-
-    return True
-
 def game():
     guess = ['_', '_', '_', '_', '_', '_']
     solution = ['p', 'y', 't', 'h', 'o', 'n']
@@ -21,27 +7,29 @@ def game():
     print('Welcome! Here is the word you have to guess:')
     print(guess)
 
-    while not check(guess, solution) and lifes > 0 :
-        line = "Please introduce a new letter: "
+    while guess != solution and lifes > 0:
+        line = raw_input("Please introduce a new letter: ")
         print(line)
 
         if len(line) != 1:
             print('You have to insert a single letter')
 
-        for i in solution:
-            if i == i in line:
+        for i, letter in enumerate(solution):
+            if letter == line:
                 print('you found a letter!')
-                i in guess == i in solution
+                guess[i] = letter
                 print(guess)
 
-        if len(line) == 1 and not solution.includes(line):
+        if len(line) == 1 and line not in solution:
             lifes -= 1
             print("Wrong letter! You only have ${lifes} left!")
 
-    if check(guess, solution):
+    if guess == solution:
         print('--> Congrats !!! You finished the game');
-        return guess
+        print(guess)
     else:
         print("--> Sorry, you have no life left, you lost the game!")
 
-game()
+
+if __name__ == '__main__':
+    game()
